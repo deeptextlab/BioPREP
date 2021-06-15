ca18371224a7173574ccd4f317fffbc044ced24e
yuheunk <yuheunk@yonsei.ac.kr>
2021-06-15 15:28:53 +0900

    code update

--- utility/simple_data_loader.py
+++ utility/simple_data_loader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 from sklearn.preprocessing import LabelEncoder
 
 def load_text_label_pairs(data_file_path):
@@ -57,3 +58,32 @@ def load_bert_data(data_file_path, label_type='predicate'): # label_type = "pred
 
     return X, y, num_classes
 
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
